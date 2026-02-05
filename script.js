e2f4f0d4981092e15d9928397b2ed97c9a5c3cc9
maha-chal <mahachalouhi@hotmail.com>
2026-02-05 23:41:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,14 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 });
+
+// Sticky navbar shadow on scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
